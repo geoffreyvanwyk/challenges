4412f7766e3a7c14ca1be685a8a009a7cff024d3
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2016-10-11 23:12:35 +0200

    happy-ladybug: Begins rewrite.

--- hacker_rank/contests/week-of-code-24/day-2-happy-ladybugs/clojure/happy-ladybugs.clj
+++ hacker_rank/contests/week-of-code-24/day-2-happy-ladybugs/clojure/happy-ladybugs.clj
@@ -1,25 +1,35 @@
 (use '[clojure.string :only (split triml)])
 
+;; (let [game-count (Integer/parseInt (read-line))]
+;;   (defn single-ladybugs?
+;;     [colors ladybugs]
+;;     (some #(= 1 (count %))
+;;           (vals (group-by #(colors %)
+;;                           ladybugs))))
+
+;;   (defn first-happy?
+;;     [ladybugs]
+;;     (= (first ladybugs)
+;;        (first (rest ladybugs))))
+
+;;   (for [i (range 1 (inc game-count))]
+;;     (let [cell-count (Integer/parseInt (read-line))
+;;           game       (read-line)
+;;           ladybugs   (filter #(not= % "_") (split game #""))
+;;           colors     (set ladybugs)]
+;;       (cond
+;;         (zero? (count ladybugs))           (conj responses "YES")
+;;         (single-ladybugs? colors ladybugs) (conj responses "NO")
+;;         (> cell-count (count ladybugs))    (conj responses "YES")
+;;         (first-happy? ladybugs)            (conj responses "YES")
+;;         :else                              (conj responses "NO")))))
+
 (let [game-count (Integer/parseInt (read-line))]
-  (defn single-ladybugs?
-    [colors ladybugs]
-    (some #(= 1 (count %))
-          (vals (group-by #(colors %)
-                          ladybugs))))
-
-  (defn first-happy?
-    [ladybugs]
-    (= (first ladybugs)
-       (first (rest ladybugs))))
-
-  (for [i (range 1 (inc game-count))]
-    (let [cell-count (Integer/parseInt (read-line))
-          game       (read-line)
-          ladybugs   (filter #(not= % "_") (split game #""))
-          colors     (set ladybugs)]
-      (cond
-        (zero? (count ladybugs))           (conj responses "YES")
-        (single-ladybugs? colors ladybugs) (conj responses "NO")
-        (> cell-count (count ladybugs))    (conj responses "YES")
-        (first-happy? ladybugs)            (conj responses "YES")
-        :else                              (conj responses "NO")))))
+  (loop [counter game-count
+         cells   []
+         games   []]
+    (if (> counter 0)
+      (recur (dec counter)
+             (conj cells (Integer/parseInt (read-line)))
+             (conj games (read-line)))
+      (apply prn games))))
