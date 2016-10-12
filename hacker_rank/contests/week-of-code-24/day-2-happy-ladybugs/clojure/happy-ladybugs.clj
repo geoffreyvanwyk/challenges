4e1e1c6e9e3fb6ef9943c3e579988857b239795f
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2016-10-12 21:23:10 +0200

    happy-ladybug: Rewrote, gives correct results in Spacemacs, but...
    ... HackerRank says my output is different.
    
    * For the example test case, I get the correct output in Spacemacs, but
      HackerRank says my output is "NO" for each game.
    * Same for test case 2.

--- hacker_rank/contests/week-of-code-24/day-2-happy-ladybugs/clojure/happy-ladybugs.clj
+++ hacker_rank/contests/week-of-code-24/day-2-happy-ladybugs/clojure/happy-ladybugs.clj
@@ -1,30 +1,17 @@
-(use '[clojure.string :only (split triml)])
-
-;; (let [game-count (Integer/parseInt (read-line))]
-;;   (defn single-ladybugs?
-;;     [colors ladybugs]
-;;     (some #(= 1 (count %))
-;;           (vals (group-by #(colors %)
-;;                           ladybugs))))
-
-;;   (defn first-happy?
-;;     [ladybugs]
-;;     (= (first ladybugs)
-;;        (first (rest ladybugs))))
-
-;;   (for [i (range 1 (inc game-count))]
-;;     (let [cell-count (Integer/parseInt (read-line))
-;;           game       (read-line)
-;;           ladybugs   (filter #(not= % "_") (split game #""))
-;;           colors     (set ladybugs)]
-;;       (cond
-;;         (zero? (count ladybugs))           (conj responses "YES")
-;;         (single-ladybugs? colors ladybugs) (conj responses "NO")
-;;         (> cell-count (count ladybugs))    (conj responses "YES")
-;;         (first-happy? ladybugs)            (conj responses "YES")
-;;         :else                              (conj responses "NO")))))
+(use '[clojure.string :only (split trim)])
 
 (let [game-count (Integer/parseInt (read-line))]
+  (defn single-ladybugs?
+    [colors ladybugs]
+    (some #(= 1 (count %))
+          (vals (group-by #(colors %)
+                          ladybugs))))
+
+  (defn first-happy?
+    [ladybugs]
+    (= (first ladybugs)
+       (first (rest ladybugs))))
+
   (loop [counter game-count
          cells   []
          games   []]
@@ -32,4 +19,15 @@
       (recur (dec counter)
              (conj cells (Integer/parseInt (read-line)))
              (conj games (read-line)))
-      (apply prn games))))
+      (loop [[c & cs] cells
+             [g & gs] games]
+        (let [ladybugs   (filter #(not= % "_") (split g #""))
+              colors     (set ladybugs)]
+         (cond
+          (zero? (count ladybugs))           (println "YES")
+          (single-ladybugs? colors ladybugs) (println "NO")
+          (> c (count ladybugs))             (println "YES")
+          (first-happy? ladybugs)            (println "YES")
+          :else                              (println "NO"))
+        (if (not (empty? cs))
+         (recur cs gs)))))))
